e2249bb8b236ae9531f4dc34475308d1065f4e95
Anthony <kaleotter@gmail.com>
2018-09-26 18:33:27 +0100

    rearranged existing files for ease of navigation. Fixed problem with item_card table not displaying correctly

--- scripts/charactersheet_render.js
+++ scripts/charactersheet_render.js
@@ -6,31 +6,31 @@
 
 
 sync.render("CHARACTER_UI", function (obj, app, scope) {
-    let html = loadTemplate("html/charsheet.html");
+    let html = loadTemplate("html/actors/pc_sheet/charsheet.html");
     let rendered = sync.render("ui_processUI")(obj, app, {display: html});
     return rendered;
 });
 
 sync.render("SKILLS_GENERAL_UI", function (obj, app, scope) {
-    let html = loadTemplate("html/skillstab_general.html");
+    let html = loadTemplate("html/actors/pc_sheet/skillstab_general.html");
     let rendered = sync.render("ui_processUI")(obj, app, {display: html});
     return rendered;
 });
 
 sync.render("SKILLS_COMBAT_UI", function (obj, app, scope) {
-    let html = loadTemplate("html/skillstab_combat.html");
+    let html = loadTemplate("html/actors/pc_sheet/skillstab_combat.html");
     let rendered = sync.render("ui_processUI")(obj, app, {display: html});
     return rendered;
 });
 
 sync.render("SKILLS_KNOW_UI", function (obj, app, scope) {
-    let html = loadTemplate("html/skillstab_know.html");
+    let html = loadTemplate("html/actors/pc_sheet/skillstab_know.html");
     let rendered = sync.render("ui_processUI")(obj, app, {display: html});
     return rendered;
 });
 
 sync.render("SKILLS_CUST_UI", function (obj, app, scope) {
-    let html = loadTemplate("html/skillstab_custom.html");
+    let html = loadTemplate("html/actors/pc_sheet/skillstab_custom.html");
     let rendered = sync.render("ui_processUI")(obj, app, {display: html});
     return rendered;
 });
@@ -43,31 +43,33 @@ sync.render("DICE_POPOUT_UI", function (obj, app, scope) {
 
 
 sync.render("WEAPON_CARD_UI", function (obj, app, scope) {
-    let html = loadTemplate("html/itemcard_weapons.html");
+    let html = loadTemplate("html/elements/item_card/itemcard_weapons.html");
     let rendered = sync.render("ui_processUI")(obj, app, {display: html});
     return rendered;
 });
 
 sync.render("ITEM_CARD_UI", function (obj, app, scope) {
-    let html = loadTemplate("html/item_card.html");
+    let html = loadTemplate("html/elements/item_card/item_card.html");
     let rendered = sync.render("ui_processUI")(obj, app, {display: html});
     return rendered;
 });
 sync.render("ARMOR_CARD_UI", function (obj, app, scope) {
-    let html = loadTemplate("html/itemcard_armor.html");
+    let html = loadTemplate("html/elements/item_card/itemcard_armor.html");
     let rendered = sync.render("ui_processUI")(obj, app, {display: html});
     return rendered;
 });
 
 sync.render("GEAR_CARD_UI", function (obj, app, scope) {
-    let html = loadTemplate("html/itemcard_gear.html");
+    let html = loadTemplate("html/elements/item_card/itemcard_gear.html");
     let rendered = sync.render("ui_processUI")(obj, app, {display: html});
     return rendered;
 });
 
 sync.render("EQUIP_TAB_UI", function (obj, app, scope) {
-    let html = loadTemplate("html/equipment_tab.html");
+    let html = loadTemplate("html/actors/pc_sheet/equipment_tab.html");
     let rendered = sync.render("ui_processUI")(obj, app, {display: html});
     return rendered;
 });
+
+
 $('body').append('<link rel="stylesheet" href="/css/css.css" type="text/css" />');
